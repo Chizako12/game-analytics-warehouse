bec52576e27b28c72c933240b22c6ca518ef0f16
Chizako12 <133034894+Chizako12@users.noreply.github.com>
2026-01-14 17:48:47 +0700

    Create fact_daily_active_users.sql

---
+++ models/marts/facts/fact_daily_active_users.sql
@@ -0,0 +1,9 @@
+SELECT
+    game_id,
+    activity_date,
+    COUNT(DISTINCT user_id) AS daily_active_users,
+    SUM(total_events) AS total_events
+FROM {{ ref('int_daily_user_activity') }}
+GROUP BY
+    game_id,
+    activity_date;
